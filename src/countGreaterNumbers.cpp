767f4175ef87ea470de02ddead01a027f07241ff
srikeerthan <srikeerthan@missionrnd.com>
2016-01-15 15:05:19 +0530

    Submit Commit

--- src/countGreaterNumbers.cpp
+++ src/countGreaterNumbers.cpp
@@ -21,5 +21,132 @@ struct transaction {
 };
 
 int countGreaterNumbers(struct transaction *Arr, int len, char *date) {
-	return -1;
+	int count=0,statements=0,equal=0;
+	while (statements < len)
+	{
+		if (Arr[statements].date[6]>date[6])
+		{
+			count = 1;
+			statements++;
+			break;
+		}
+		else if (Arr[statements].date[6] == date[6])
+		{
+			if (Arr[statements].date[7] > date[7])
+			{
+				count++;
+				statements++;
+				break;
+			}
+			else if (Arr[statements].date[7] == date[7])
+			{
+				if(Arr[statements].date[8] > date[8])
+				{
+					count++;
+					statements++;
+					break;
+				}
+				else if (Arr[statements].date[8] == date[8])
+				{
+					if (Arr[statements].date[9] > date[9])
+					{
+						count++;
+						statements++;
+						break;
+					}
+					else if (Arr[statements].date[9] == date[9])
+					{
+						if (Arr[statements].date[3] > date[3])
+						{
+							count++;
+							statements++;
+							break;
+						}
+						else if (Arr[statements].date[3] == date[3])
+						{
+							if (Arr[statements].date[4] > date[4])
+							{
+								count++;
+								statements++;
+								break;
+							}
+							else if (Arr[statements].date[4] == date[4])
+							{
+								if (Arr[statements].date[0] > date[0])
+								{
+									count++;
+									statements++;
+									break;
+								}
+								else if (Arr[statements].date[0] == date[0])
+								{
+									if (Arr[statements].date[1] > date[1])
+									{
+										count++;
+										statements++;
+										break;
+									}
+									else if (Arr[statements].date[1] == date[1])
+									{
+										equal = 1;
+										statements++;
+										continue;
+									}
+									else
+									{
+										statements++;
+										continue;
+									}
+								}
+								else
+								{
+									statements++;
+									continue;
+								}
+							}
+							else
+							{
+								statements++;
+								continue;
+							}
+						}
+						else
+						{
+							statements++;
+							continue;
+						}
+					}
+					else
+					{
+						statements++;
+						continue;
+					}
+				}
+				else
+				{
+					statements++;
+					continue;
+				}
+			}
+			else
+			{
+				statements++;
+				continue;
+			}
+		}
+		else
+		{
+			statements++;
+			continue;
+		}
+	}
+	if (count == 1)
+	{
+		while (statements < len)
+		{
+			count++;
+			statements++;
+		}
+	}
+	return count;
 }
